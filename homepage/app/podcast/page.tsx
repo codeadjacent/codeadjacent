a13d316699eee7e1c1976b20c322689d23fd8cbf
Han Che <hanche2001@gmail.com>
2025-10-31 16:33:08 +0100

    feat(homepage): add some pages with dummy content

--- homepage/app/podcast/page.tsx
+++ homepage/app/podcast/page.tsx
@@ -1,13 +1,30 @@
 import Link from "next/link";
+import {Code} from "lucide-react";
 
 export default function PodcastPage() {
     return (
-        <main>
-            <h1>Podcast</h1>
-            <ul>
-                <li><Link href="/podcast/1">Episode 1</Link></li>
-                <li><Link href="/podcast/2">Episode 2</Link></li>
-            </ul>
+        // <main>
+        //     {/*<h1>Podcast</h1>*/}
+        //     {/*<ul>*/}
+        //     {/*    <li><Link href="/podcast/1">Episode 1</Link></li>*/}
+        //     {/*    <li><Link href="/podcast/2">Episode 2</Link></li>*/}
+        //     {/*</ul>*/}
+        //     <main className="flex justify-center items-center flex-1">
+        //         <div className="text-center">
+        //             <Code size={200} />
+        //             <h1>
+        //                 Work in progress
+        //             </h1>
+        //         </div>
+        //     </main>
+        // </main>
+        <main className="flex justify-center items-center flex-1">
+            <div className="text-center">
+                <Code size={200}/>
+                <h1>
+                    Podcast work in progress
+                </h1>
+            </div>
         </main>
     )
 }
---
+++ homepage/components/server/header/header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import RouterLink from "@/components/client/router-link";
+
+export default function Header() {
+    return (
+        <div className="navbar sticky top-4 bg-base-100 shadow-sm p-0 min-h-0">
+            <div className="navbar-start w-min">
+
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h8m-8 6h16"/>
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={-1}
+                        className="menu menu-sm dropdown-content bg-base-100 [&_li>*]:rounded-none rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><RouterLink href="/">Home</RouterLink></li>
+                        <li><RouterLink href="/podcast">Podcast</RouterLink></li>
+                        <li><RouterLink href="/about">About</RouterLink></li>
+                    </ul>
+                </div>
+                <Link href="/" className="btn btn-ghost text-xl">CodeAdjacent</Link>
+            </div>
+            <div className="navbar-end w-full">
+                <ul className="menu menu-horizontal gap-2 [&_li>*]:rounded-none hidden lg:flex">
+                    <li><RouterLink href="/">Home</RouterLink></li>
+                    <li><RouterLink href="/podcast">Podcast</RouterLink></li>
+                    <li><RouterLink href="/about">About</RouterLink></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
